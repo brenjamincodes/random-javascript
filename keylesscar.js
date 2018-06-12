4f32e44deb3904c274a1cbc8f3b3f40d7c6fc790
Benjamin Winokur <blwinokur@me.com>
2018-06-11 20:10:19 -0700

    age===18 fix
    Fixed last else if statement to display the correct alert.

---
+++ keylesscar.js
@@ -0,0 +1,8 @@
+var age = prompt("How old are you?");
+if (Number(age) < 18) { 
+alert("Sorry, you are too young to drive this car. Powering Off");
+}else if (Number(age) > 18) {
+alert("Powering On. Enjoy the ride!");}
+else if (Number(age) === 18) {
+alert("Congratulations on your first year of driving. Enjoy the ride!");
+}
